a1f272cb8eb0c04b3df507d59b5b3ffc1ec6357b
Kyle <45010713+kylekrny@users.noreply.github.com>
2024-08-07 23:02:34 -0400

    setup post command

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ app.get('/', (req, res) => {
     mailSender().catch(console.error)
 })
 
+
+app.post('/quote', (req,res) => {
+    mailSender().then(res.sendStatus(200)).catch(console.error)
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
