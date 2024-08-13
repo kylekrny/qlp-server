0663ac119119abade91d642b9b42e7be8ee31e0b
Kyle <45010713+kylekrny@users.noreply.github.com>
2024-08-13 11:45:23 -0400

    initialize firbase-admin

--- service/firestore.js
+++ service/firestore.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc, updateDoc, getDoc, doc } from "firebase/firestore";
+import { initializeApp } from "firebase-admin/app";
+import { getFirestore, collection, addDoc, updateDoc, getDoc, doc } from "firebase-admin/firestore";
 
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
