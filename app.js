67e25316f0e1be6487f8f8653fa80baf488b64f0
Kyle <45010713+kylekrny@users.noreply.github.com>
2024-08-14 23:19:53 -0400

    minor tweaks to email formatting

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 import express from "express";
 import { attachmentLogic, mailSender } from "./service/mail.js";
-import { auth, requiredScopes } from "express-oauth2-jwt-bearer";
 import dotenv from "dotenv";
 import { readDocument, updateDocument, writeGenericDocument } from "./service/firestore.js";
 import { generateAuthParams } from "./service/photo.js";
-import { verifyAuth } from "./middleware/auth.js";
 
 dotenv.config();
 const app = express();
@@ -19,37 +17,40 @@ app.use(express.json())
 const port = 3000;
 
 
-const abandonedEmailOptions = (reqData) => { return {
-    to: "kyledkearney@gmail.com",
-    replyTo: reqData.email,
-    subject: `Abandoned quote from QualityLapelpins.com`,
-    text: `Name: ${reqData.name || "n/a"} \n Email: ${reqData.email} \n Product: ${reqData.product || "n/a"} \n Size: ${reqData.size || "n/a"} \n location: ${reqData.shippingLocation || "n/a"} \n, message: ${reqData.message || "n/a"}`,
-}}
+const abandonedEmailOptions = (reqData) => {
+    return {
+        to: process.env.NOTIFICATION_EMAIL,
+        replyTo: reqData.email,
+        subject: `Abandoned Quote from QualityLapelpins.com`,
+        text: `Name: ${reqData.name || "n/a"} \n Email: ${reqData.email} \n Product: ${reqData.product || "n/a"} \n Size: ${reqData.size || "n/a"} \n location: ${reqData.shippingLocation || "n/a"} \n, message: ${reqData.message || "n/a"}`,
+        html: `<p>Name: ${reqData.name || "n/a"}</p><p>Email: ${reqData.email || "n/a"}</p><p>Product: ${reqData.product || "n/a"}</p><p>Size: ${reqData.size || "n/a"}</p><p>Location: </p><p>Message: ${reqData.message || "n/a"}</p>`
+    }
+}
 
 const abandonedEmail = (data, id) => {
-        mailSender(abandonedEmailOptions(data)).then(() => {
-            updateDocument("quotes", id, {abandoned: true, notificationSent: Date.now()})
-        }).catch((err) => {
-            console.error(err)
-        })
+    mailSender(abandonedEmailOptions(data)).then(() => {
+        updateDocument("quotes", id, { abandoned: true, notificationSent: Date.now() })
+    }).catch((err) => {
+        console.error(err)
+    })
 }
 
 app.get('/', (req, res) => {
-    res.send({message: 'The app is running...'});
+    res.send({ message: 'The app is running...' });
 });
 
-app.post('/quote', (req,res) => {
+app.post('/quote', (req, res) => {
     const reqData = req.body;
 
     writeGenericDocument("quotes", reqData).then((docRef) => {
-        res.status(200).send(JSON.stringify({docId: docRef}))
+        res.status(200).send(JSON.stringify({ docId: docRef }))
     }).catch((error) => {
         //Admin notification
         res.status(500).send(JSON.stringify(error));
     })
 });
 
-app.get('/upload', (req,res) => {
+app.get('/upload', (req, res) => {
     generateAuthParams().then((data) => {
         res.status(200).send(JSON.stringify(data));
     })
@@ -59,15 +60,16 @@ app.put('/quote/:id', (req, res) => {
     updateDocument("quotes", req.params.id, req.body).then((data) => {
         if (req.body.submitted) {
             readDocument("quotes", req.params.id).then((data) => {
-            
+
                 const userEmailOptions = {
                     to: process.env.NOTIFICATION_EMAIL,
                     replyTo: data.email,
-                    subject: `${data.product} quote from Qualitylapelpins.com`,
+                    subject: `${data.product} Quote from Qualitylapelpins.com`,
                     text: `Name: ${data.name || "n/a"} \n Email: ${data.email || "n/a"} \n Product: ${data.product || "n/a"} \n Size: ${data.size || "n/a"} \n location: ${data.shippingLocation || "n/a"} \n message: ${data.message || "n/a"}`,
-                    ...attachmentLogic(data.image1, data.image2),    
+                    html: `<p>Name: ${data.name || "n/a"}</p><p>Email: ${data.email || "n/a"}</p><p>Product: ${data.product || "n/a"}</p><p>Size: ${data.size || "n/a"}</p><p>Location: </p><p>Message: ${data.message || "n/a"}</p>`,
+                    ...attachmentLogic(data.image1, data.image2),
                 }
-                
+
                 const requesterEmailOptions = {
                     to: data.email,
                     replyTo: "info@qualitylapelpins.com",
@@ -75,23 +77,23 @@ app.put('/quote/:id', (req, res) => {
                     text: "Thank you for submitting a quote with Quality Lapel Pins we will reach out as soon as possible",
                     html: "<b>Thank you for submitting a quote with Quality Lapel Pins we will reach out as soon as possible</b>"
                 }
-                
-                
+
+
                 mailSender(userEmailOptions).then(() => {
-                    res.status(200).send({message: "Your quote has been successfully sent!"});
+                    res.status(200).send({ message: "Your quote has been successfully sent!" });
                     // mailSender(requesterEmailOptions);
                 })
-                .catch(() => {
-                    console.error;
-                    res.status(500).send({message: "Your message failed to send please contact Quality Lapel Pins"});
-                }); 
-        })
+                    .catch(() => {
+                        console.error(error);
+                        res.status(500).send({ message: "Your message failed to send please contact Quality Lapel Pins" });
+                    });
+            })
         } else {
-            res.status(200).send({message: "Record successfully updated."});
+            res.status(200).send({ message: "Record successfully updated." });
         }
     }).catch((error) => {
-        console.error(error)
-        res.status(500).send(error)
+        console.error(error);
+        res.status(500).send(error);
     })
 });
 
